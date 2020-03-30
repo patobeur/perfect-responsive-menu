ec09eb5b72fd5e2d1cf180904da6507bd353ace0
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2020-03-30 14:02:57 +0200

    rohhhhhhhhhhh vilain

--- navigation.js
+++ navigation.js
@@ -1,9 +1,13 @@
 const toggleMenu = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
 const monbeautifooolbody = document.querySelector('body');
+
 toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
 	monbeautifooolbody.classList.add("noscroll");
+});
+menu.addEventListener('click', function(e) {
+	console.log('event ' + e.target);
 });
