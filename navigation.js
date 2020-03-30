567e3dfe364516d55998b03c06870ebb051621fa
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2020-03-30 13:23:35 +0200

    class noscroll ajouté (au body) au clique (sur les menus) (exercice : BL)

--- navigation.js
+++ navigation.js
@@ -1,8 +1,9 @@
 const toggleMenu = document.querySelector('.navigation button');
 const menu = document.querySelector('.navigation ul');
-
+const monbeautifooolbody = document.querySelector('body');
 toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+	monbeautifooolbody.classList.add("noscroll");
 });
